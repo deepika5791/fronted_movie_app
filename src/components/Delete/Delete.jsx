61493e3f7cdc45dcfa00af8dbe9cdf822939769a
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-29 21:31:57 +0530

    id correction

--- src/components/Delete/Delete.jsx
+++ src/components/Delete/Delete.jsx
@@ -7,7 +7,7 @@ const Delete = ({ id, onDelete }) => {
   const handleDelete = async () => {
     try {
       await axios.delete(`https://movieapi-1-txwt.onrender.com/movies/${id}`);
-      onDelete(_id);
+      onDelete(id);
 
       window.dispatchEvent(new CustomEvent("movieDeleted", { detail: { id } }));
     } catch (error) {
